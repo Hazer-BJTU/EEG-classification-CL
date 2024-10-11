a8cc663bb4165f8da2ed1710db11a77000c2cc30
Hazer <3169443421@qq.com>
2024-10-11 19:25:58 +0800

    2024/10/11-lcz-generator

--- generator.py
+++ generator.py
@@ -21,28 +21,6 @@ class Label2Vec(nn.Module):
         return output
 
 
-class GRUlayer(nn.Module):
-    def __init__(self, input_size, hiddens, layers, dropout=0, **kwargs):
-        super(GRUlayer, self).__init__(**kwargs)
-        self.input_size = input_size
-        self.hiddens = hiddens
-        self.layers = layers
-        self.dropout = dropout
-        self.network = nn.GRU(input_size, hiddens, num_layers=layers, batch_first=True,
-                              dropout=dropout, bidirectional=True)
-
-    def get_initial_states(self, batch_size, device):
-        return torch.zeros((2 * self.layers, batch_size, self.hiddens), device=device)
-
-    def forward(self, X):
-        batch_size, seq_length = X.shape[0], X.shape[1]
-        H0 = self.get_initial_states(batch_size, X.device)
-        (output, Hn) = self.network(X, H0)
-        if not output.is_contiguous():
-            output = output.contiguous()
-        return output
-
-
 class CNNlayer(nn.Module):
     def __init__(self, channels_lst, kernels_lst, **kwargs):
         super(CNNlayer, self).__init__(**kwargs)
@@ -65,73 +43,132 @@ class CNNlayer(nn.Module):
         return X
 
 
+class ShortTermGRU(nn.Module):
+    def __init__(self, input_size, hiddens, **kwargs):
+        super(ShortTermGRU, self).__init__(**kwargs)
+        self.input_size = input_size
+        self.hiddens = hiddens
+        self.block = nn.GRU(input_size, hiddens, batch_first=True, bidirectional=True)
+
+    def get_initial_states(self, batch_size, device):
+        return torch.zeros((2, batch_size, self.hiddens), device=device)
+
+    def forward(self, X):
+        batch_size, seq_length, F, T = X.shape[0], X.shape[1], X.shape[2], X.shape[3]
+        X = X.transpose(2, 3).view(batch_size * seq_length, T, F)
+        H0 = self.get_initial_states(X.shape[0], X.device)
+        (output, Hn) = self.block(X, H0)
+        if not output.is_contiguous():
+            output = output.contiguous()
+        output = output.view(batch_size, seq_length, T, self.hiddens * 2)
+        return output
+
+
+class LongTermGRU(nn.Module):
+    def __init__(self, input_size, hiddens, **kwargs):
+        super(LongTermGRU, self).__init__(**kwargs)
+        self.input_size = input_size
+        self.hiddens = hiddens
+        self.block = nn.GRU(input_size, hiddens, batch_first=True, bidirectional=True)
+
+    def get_initial_states(self, batch_size, device):
+        return torch.zeros((2, batch_size, self.hiddens), device=device)
+
+    def forward(self, X):
+        batch_size, seq_length, features = X.shape[0], X.shape[1], X.shape[2]
+        H0 = self.get_initial_states(batch_size, X.device)
+        (output, Hn) = self.block(X, H0)
+        if not output.is_contiguous():
+            output = output.contiguous()
+        return output
+
+
 class Encoder(nn.Module):
-    def __init__(self, channels_num, **kwargs):
+    def __init__(self, channels_num=2, **kwargs):
         super(Encoder, self).__init__(**kwargs)
         self.label2vec = Label2Vec(64)
-        self.cnn = CNNlayer([channels_num * 129, 128, 128, 64, 32], [5, 5, 5, 5])
-        self.linear = nn.Sequential(nn.Linear(864, 512), nn.ReLU())
-        self.rnn = GRUlayer(512, 256, 2)
-        self.mu = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
-        self.sigma = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
-        self.invariant = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 256))
+        self.cnn = CNNlayer((channels_num * 129, 128, 64, 32), (3, 3, 3))
+        self.long_term_gru = LongTermGRU(864, 128)
+        self.short_term_gru = ShortTermGRU(channels_num * 129, 128)
+        self.mu = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256)
+        )
+        self.sigma = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256)
+        )
 
     def forward(self, X, y):
+        batch_size, seq_length, F, T = X.shape[0], X.shape[1], X.shape[2], X.shape[3]
         y = self.label2vec(y)
-        X = self.cnn(X).view(X.shape[0], X.shape[1], -1)
-        X = torch.cat((X, y), dim=2)
-        X = self.linear(X)
-        X = self.rnn(X)
-        output1, output2, output3 = self.mu(X), self.sigma(X), self.invariant(X)
-        return output1, output2, output3
+        ld = self.cnn(X)
+        ld = ld.view(batch_size, seq_length, -1)
+        ld = torch.cat((ld, y), dim=2)
+        ld = self.long_term_gru(ld)
+        ld = torch.unsqueeze(ld, dim=2)
+        ld = ld.expand(batch_size, seq_length, T, ld.shape[3])
+        sd = self.short_term_gru(X)
+        Z = torch.cat((sd, ld), dim=3)
+        Z = Z.view(batch_size * seq_length * T, -1)
+        output1, output2 = self.mu(Z), self.sigma(Z)
+        output1 = output1.view(batch_size, seq_length, T, -1)
+        output2 = output2.view(batch_size, seq_length, T, -1)
+        return output1, output2
 
 
 class Decoder(nn.Module):
     def __init__(self, channels_num, **kwargs):
         super(Decoder, self).__init__(**kwargs)
         self.label2vec = Label2Vec(64)
-        self.rnn = GRUlayer(448, 256, 2)
-        self.linear = nn.Sequential(nn.Linear(512, 800), nn.ReLU())
-        self.cnn = CNNlayer([32, 64, 128, 256, channels_num * 129], [5, 5, 5, 5])
+        self.cnn = CNNlayer((256, 128, 64, 32), (3, 3, 3))
+        self.long_term_gru = LongTermGRU(864, 128)
+        self.short_term_gru = ShortTermGRU(256, 128)
+        self.linear = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, channels_num * 129)
+        )
 
     def forward(self, X, y):
+        batch_size, seq_length, F, T = X.shape[0], X.shape[1], X.shape[2], X.shape[3]
         y = self.label2vec(y)
-        X = torch.cat((X, y), dim=2)
-        X = self.rnn(X)
-        X = self.linear(X)
-        X = X.view(X.shape[0], X.shape[1], 32, 25)
-        X = self.cnn(X)
-        return X
+        ld = self.cnn(X)
+        ld = ld.view(batch_size, seq_length, -1)
+        ld = torch.cat((ld, y), dim=2)
+        ld = self.long_term_gru(ld)
+        ld = torch.unsqueeze(ld, dim=2)
+        ld = ld.expand(batch_size, seq_length, T, ld.shape[3])
+        sd = self.short_term_gru(X)
+        Z = torch.cat((sd, ld), dim=3)
+        Z = Z.view(batch_size * seq_length * T, -1)
+        output = self.linear(Z)
+        output = output.view(batch_size, seq_length, T, -1).transpose(2, 3)
+        return output
 
 
 class CVAE(nn.Module):
-    def __init__(self, channels_num, **kwargs):
+    def __init__(self, channels_num=2, **kwargs):
         super(CVAE, self).__init__(**kwargs)
         self.encoder = Encoder(channels_num)
         self.decoder = Decoder(channels_num)
 
     def forward(self, X, y, z):
-        mu, sigma, invariant = self.encoder(X, y)
-        Z = mu + sigma.exp() * z
-        Z = torch.cat((Z, invariant), dim=2)
-        X_fake = self.decoder(Z, y)
-        return X_fake, mu, sigma, invariant
+        mu, sigma = self.encoder(X, y)
+        Z = mu + z * sigma.exp()
+        Z = Z.transpose(2, 3)
+        Z = self.decoder(Z, y)
+        return Z, mu, sigma
 
 
 if __name__ == '__main__':
     X = torch.randn((16, 10, 258, 25), dtype=torch.float32, device='cpu', requires_grad=False)
     y = torch.randint(0, 5, (16, 10), dtype=torch.int64, device='cpu', requires_grad=False)
-    z = torch.randn((16, 10, 128), dtype=torch.float32, device='cpu', requires_grad=False)
-    encoder = Encoder(2)
-    decoder = Decoder(2)
-    mu, sigma, invariant = encoder(X, y)
-    Z = mu + z * sigma.exp()
-    Z = torch.cat((Z, invariant), dim=2)
-    X_fake = decoder(Z, y)
-    print(X_fake.shape)
-    torch.save(encoder.state_dict(), 'cvae_encoder.pth')
-    torch.save(decoder.state_dict(), 'cvae_decoder.pth')
-    cvae = CVAE(2)
-    X_fake, mu, sigma, invariant = cvae(X, y, z)
-    print(X_fake.shape, mu.shape, sigma.shape)
-    torch.save(cvae.state_dict(), 'cvae_network.pth')
+    z = torch.randn((16, 10, 25, 256), dtype=torch.float32, device='cpu', requires_grad=False)
+    net = CVAE()
+    Z, mu, sigma = net(X, y, z)
+    print(Z.shape, mu.shape, sigma.shape)
+    torch.save(net.encoder.state_dict(), 'cvae_encoder.pth')
+    torch.save(net.decoder.state_dict(), 'cvae_decoder.pth')
